8e6f4446f36e87369ac4bdedc8382a616e6bdd53
Crystaliner1000 <honeygeeh27@gmail.com>
2023-11-16 18:24:06 +0100

    fixing header issues

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,30 @@
+
+// Header.jsx
+
 import React, { useState } from 'react';
 import styles from './header.module.css';
 
 const Header = () => {
-  const [showNav, setShowNav] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-  const toggleNav = () => {
-    setShowNav(!showNav);
+  const handleToggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
   };
 
   return (
     <header className={styles.header}>
       <div className={styles.logo}>NeNorvalls</div>
-      <div className={styles.nav}>
- 
-        <div className={`${styles.menuIcon} ${showNav ? styles.close : ''}`} onClick={toggleNav}>
-          <div className={styles.hamburgerLine}></div>
-          <div className={styles.hamburgerLine}></div>
-          <div className={styles.hamburgerLine}></div>
-        </div>
-        
-        <nav className={`${styles.navItems} ${showNav ? styles.show : ''}`}>
-          <a href="/">Home</a>
-          <a href="/projects">Projects</a>
-        </nav>
+      <div className={styles.menuIcon} onClick={handleToggleMenu}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
       </div>
+      <nav className={`${styles.nav} ${isMenuOpen ? styles.open : ''}`}>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/projects">Projects</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
